87a8620be256b56219d0f2b480300f374af13278
wizage <wizagesmax@gmail.com>
2019-07-22 07:49:19 -0700

    Initial Commit

---
+++ twitchConferences/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  twitchConferences
+//
+//  Created by Sam Patzer on 7/22/19.
+//  Copyright © 2019 wizage. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
